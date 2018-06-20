a2d1fcd90090d91a360f8db03f94507e5584ed56
David Toth <dtoth1@nd.edu>
2018-06-20 16:36:23 -0500

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Stocks
+This code is a start-up built purely on Python 3.x to organize, visualize, manipulate, and predict stock market values and data.
+
+## Authors
+* David W. Toth (dtoth1@nd.edu), Co-founder
+* Jonathan D. Burns (jonaburns@iu.edu), Co-founder
+
+## License: MIT License  
+You agree to the terms listed under LICENSE 
--- sample_graph.py
+++ sample_graph.py
@@ -1,7 +1,15 @@
 """ Script imports stock data and presents
 	them as graphs """
 
+import time
+import datetime as dt
+now = dt.datetime.now()
+t0 = time.clock()
+#Imports packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 print("Packages imported")
+t1 = time.clock()
+total_time = t1-t0
+print("Script time:", total_time)
