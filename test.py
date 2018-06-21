1a2ddaf3b957a6b1a054dbd0d4a30e57e1cd9996
David Toth <dtoth1@nd.edu>
2018-06-21 14:46:01 -0500

    Functional sorting of tickers

--- test.py
+++ test.py
@@ -1,19 +1,34 @@
 import time
 import datetime as dt
 import pandas as pd
-import pandas_datareader.data as pdr
+import pandas_datareader.data as web
 import numpy as np
 import matplotlib.pyplot as plt
-tickers = ['AAPL', 'AMD', 'SBUX']
+tickers = ['AMD']
+working_tickers = []
+path = 'C:/Users/david/Documents/tickers.xlsx'
+df = pd.read_excel(path)
+print(df['Ticker'])
+list = df['Ticker'].tolist()
+for i in list:
+	tickers.append(str(i))
 def function(tickers):
 	for i in tickers:
-		x = pdr.get_data_robinhood(i, start=dt.datetime(2012, 6, 20), end=dt.datetime(2018, 6, 20))
-		del x['session']
-		x = x.astype(float)
-		print(x)
-		x['close_price'].plot()
-		# plt.show()
-		summary = x.describe()
-		print(summary)
+		try:
+			x = web.DataReader(i, 'robinhood')
+			del x['session']
+			x = x.astype(float)
+			print(x)
+			x['close_price'].plot()
+			# plt.show()
+			summary = x.describe()
+			print(summary)
+			new_tickers.append(i)
+			print(new_tickers)
+			new_df = pd.DataFrame({'col': working_tickers})
+			new_df.to_csv('workingtickers.csv')
+		except:
+			print("Failed")
+			pass
 data = function(tickers)
 print(data)
