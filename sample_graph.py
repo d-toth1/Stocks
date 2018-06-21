71170ab7e90a9dba33545d3149cd4a8b84167795
David Toth <dtoth1@nd.edu>
2018-06-20 20:34:44 -0500

    Updates

--- sample_graph.py
+++ sample_graph.py
@@ -3,13 +3,14 @@
 
 import time
 import datetime as dt
+import os
+width = os.get_terminal_size().columns
 
 ''' Import Packages '''
 import pandas as pd
 import pandas_datareader.data as pdr
 import numpy as np
 import matplotlib.pyplot as plt
-print("Packages imported")
 
 ''' Import Stock Data from Robinhood '''
 aapl = pdr.get_data_robinhood('AAPL', start=dt.datetime(2017, 6, 20), end=dt.datetime(2018, 6, 20))
@@ -17,7 +18,6 @@ del aapl['session']
 
 ''' Export data '''
 aapl.to_csv('aapl.csv')
-print('Data Exported')
 
 ''' Plot past year of data '''
 aapl= aapl.astype(float)
@@ -26,7 +26,12 @@ plt.xlabel('Time')
 plt.ylabel('Price')
 plt.title('AAPL Closing Prices')
 
+''' Gather Statistics on Stock '''
+summary = aapl.describe()
+print('********** Statistical Summary **********')
+print(summary)
+print('\n')
 print(dt.datetime.now())
 print('Created by D.W. Toth and J.D. Burns')
 
-plt.show()
+# plt.show()
